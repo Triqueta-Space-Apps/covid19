158ac77213de9ac29d4aae2c38e6cc7e30714d1d
ArthurAMP <arthurmpapp@gmail.com>
2020-05-31 18:14:47 -0300

    post routes created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const usersController = require('./controllers/userControllers');
+const usersController = require('./controllers/usersController');
+const postsController = require('./controllers/postsController');
 const routes = express.Router();
 
 routes.post('/registrar', usersController.register);
 routes.get('/users', usersController.list);
 routes.post('/login', usersController.login);
+
+routes.get('/posts', postsController.posts);
+routes.post('/create', postsController.create);
+
+
 module.exports = routes;
