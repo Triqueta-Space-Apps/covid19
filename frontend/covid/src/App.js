a0bd981028f802e4a16cdd388e8e53dc9bbf91fb
Larissa Santos Azevedo <larissa.santosazevedo@hotmail.com>
2020-05-31 18:19:35 -0300

    adding lib geolocated

--- frontend/covid/src/App.js
+++ frontend/covid/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Demo from './demo.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        k
-          Learn React
-        </a>
-      </header>
+      <Demo teste={"nina"}/>
     </div>
   );
 }
