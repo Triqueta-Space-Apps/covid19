e4aefca7be8b32de06f40e16c68ca69e385d2911
ArthurAMP <arthurmpapp@gmail.com>
2020-06-01 19:46:52 -0300

    added map in frontpage

--- frontend/covid/src/App.js
+++ frontend/covid/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Demo from './demo.js';
 import Timeline from './Timeline';
 import Welcome from './Welcome';
+import Map from './map/';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <Map />
       <Demo />
       <Timeline />
     </div>
--- frontend/covid/src/map/index.js
+++ frontend/covid/src/map/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
 import './styles.css';
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
@@ -7,7 +7,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 function Map(){
     return(
         <div>
-            <GoogleMap defaultZoom = {15} defaultCenter = {{lat: 12, lng: 12}} />
+            <GoogleMap defaultZoom = {4.5} defaultCenter = {{lat: 31, lng: -100}}> <Marker position={{ lat: 31, lng: -100}} /></GoogleMap>
         </div>
     );
 }
@@ -17,7 +17,7 @@ export default function Mapa(){
     return <map className = 'Map'>
         <WrappedMap googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`} 
         loadingElement={<div style= {{height: `100%`}} />}
-        containerElement =  {<div style={{ height: `400px` }} />}
+        containerElement =  {<div style={{ height: `200px` }} />}
         mapElement = {<div style={{ height: `100%` }} />}
         />
         
